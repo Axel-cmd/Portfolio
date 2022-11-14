8f7bb940f5dada96b14dfe32e0cbdfa7427b6433
AXELMINART <77530883+Axel-cmd@users.noreply.github.com>
2022-11-14 09:16:25 +0100

    mon site web

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 window.addEventListener("DOMContentLoaded", () => {
 
+    const hambMenu = document.getElementById("hamb-menu");
+    const navMenuTel = document.getElementById("navMenuTel");
 
+    hambMenu.addEventListener('click', () => {
+        navMenuTel.style.right = 0;
+    })
 
+    const closeNavMenuTel = document.getElementById('close-wrapper');
 
-
-
-
-
-
-
+    closeNavMenuTel.addEventListener('click', () => {
+        navMenuTel.style.right = "-100%";
+    })
     
 })
